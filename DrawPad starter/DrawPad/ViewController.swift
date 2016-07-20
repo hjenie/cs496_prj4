97a1fe30d3e2ede54fb3efbeebd0e00b7da883d6
user <user@userui-iMac-3.local>
2016-07-20 19:46:10 +0900

    Zoom...

--- DrawPad starter/DrawPad/ViewController.swift
+++ DrawPad starter/DrawPad/ViewController.swift
@@ -8,7 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PassTouchesScrollView: UIScrollView {
+    
+    var delegatePass : UIViewController?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("scrollBegan")
+        // Notify it's delegate about touched
+        self.delegatePass?.touchesBegan(touches, withEvent: event)
+        
+        if self.dragging == true {
+            self.nextResponder()?.touchesBegan(touches, withEvent: event)
+        } else {
+            super.touchesBegan(touches, withEvent: event)
+        }
+        
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        super.touchesCancelled(touches, withEvent: event)
+        print("cancled")
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("scrollMoved")
+        print("zoomscale", self.zoomScale)
+        //print("contentOffset" + "%f %f",self.contentOffset.x,self.contentOffset.y)
+        // Notify it's delegate about touched
+        self.delegatePass?.touchesMoved(touches, withEvent: event)
+        
+        if self.dragging == true {
+            self.nextResponder()?.touchesMoved(touches, withEvent: event)
+        } else {            
+            super.touchesMoved(touches, withEvent: event)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("scrollEnded")
+        self.delegatePass?.touchesEnded(touches, withEvent: event)
+        
+        if self.dragging == true {
+            self.nextResponder()?.touchesEnded(touches, withEvent: event)
+        } else {
+            super.touchesEnded(touches, withEvent: event)
+        }
+    }
+}
+
+class ViewController: UIViewController, UIScrollViewDelegate { // UIScrollViewDelegate
     
   var lastPoint = CGPoint.zero
   var red: CGFloat = 0.0
@@ -18,19 +74,29 @@ class ViewController: UIViewController {
   var opacity: CGFloat = 1.0
   var swiped = false
 
+
+  @IBOutlet weak var scrollView: PassTouchesScrollView!
+  @IBOutlet weak var drawView: UIView!
   @IBOutlet weak var mainImageView: UIImageView!
   @IBOutlet weak var tempImageView: UIImageView!
+    
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    scrollView.delegate = self
+    scrollView.delegatePass = self
+    
+    //[self.scrollView.layer setAnchorPoint,:CGPointMake(300, 300)];
+
+    //print("it is ",scrollView.contentSize.width / 2, scrollView.contentSize.height / 2)
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+    
   // MARK: - Actions
 
   @IBAction func reset(sender: AnyObject) {
@@ -79,12 +145,14 @@ class ViewController: UIViewController {
         let context = UIGraphicsGetCurrentContext()
         tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
         
+        //self.scrollView.contentOffset.x + fromPoint.x * scrollView.zoomScale
+        print("zoomscale : ", scrollView.zoomScale)
         // 2
-        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
-        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        CGContextMoveToPoint(context, (self.scrollView.contentOffset.x + fromPoint.x) / scrollView.zoomScale, (self.scrollView.contentOffset.y + fromPoint.y) / scrollView.zoomScale)
+        CGContextAddLineToPoint(context, (self.scrollView.contentOffset.x + toPoint.x) / scrollView.zoomScale, (self.scrollView.contentOffset.y + toPoint.y) / scrollView.zoomScale)
         
         // 3
-        CGContextSetLineCap(context, CGLineCap.Butt)
+        CGContextSetLineCap(context, CGLineCap.Round)
         CGContextSetLineWidth(context, brushWidth)
         CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
         CGContextSetBlendMode(context, CGBlendMode.Normal)
@@ -112,6 +180,8 @@ class ViewController: UIViewController {
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        print("Ended")
+        print("it is ",scrollView.contentSize.width / 2, scrollView.contentSize.height / 2)
         
         if !swiped {
             // draw a single point
@@ -126,6 +196,19 @@ class ViewController: UIViewController {
         UIGraphicsEndImageContext()
         
         tempImageView.image = nil
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        print("viewForZomming")
+        print("before center : ",self.drawView.center)
+        let screenRect = UIScreen.mainScreen().bounds
+        let screenWidth = screenRect.size.width
+        let screenHeight = screenRect.size.height
+        //self.drawView.center = CGPointMake(screenWidth / 2, screenHeight / 2)
+        print("after center : ",self.drawView.center)
+        //self.scrollView.contentOffset = CGPoint(x:-50, y:-50)
+        print("offset : ",self.scrollView.contentOffset)
+        return self.drawView
     }
     
     let colors: [(CGFloat, CGFloat, CGFloat)] = [
